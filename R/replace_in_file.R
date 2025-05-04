ab85015deec9c40162d6febd84f1562b1c10716a
Daniel Vartanian <danvartan@gmail.com>
2025-05-04 02:16:58 -0300

    Change replace_in_file() parameters and code

--- R/replace_in_file.R
+++ R/replace_in_file.R
@@ -7,77 +7,78 @@
 #' `replace_in_file()` searches each line of a file and replaces a specific
 #' pattern when found.
 #'
-#' @param dir (Optional) a string indicating the directory of the files.
-#'   Defaults to clipboard content.
-#' @param pattern A string indicating the pattern to look for. The default
-#'   interpretation is a regular expression. This parameter will be used on
-#'   [stringr::str_replace_all()].
-#' @param replacement A string indicating the replacement value. This parameter
-#'   will be used on [stringr::str_replace_all()].
+#' @param file A [`character`][base::character()] vector specifying the
+#'   path(s) to the file(s) in which to perform the replacement.
+#' @param pattern A [`character`][base::character()] vector specifying the
+#'   pattern(s) (regular expressions) to search for. If multiple patterns are
+#'   provided, each must correspond to a replacement of the same length.
+#' @param replacement A [`character`][base::character()] vector specifying the
+#'   replacement value(s) to use. Each element is applied to the corresponding
+#'   pattern via [str_replace_all()][stringr::str_replace_all()].
 #'
 #' @return An invisible `NULL`. This function is used for its side effect.
-#'
-#' @family string functions
 #'
 #' @family file functions
 #' @export
 #'
 #' @examples
-#' file_name <- tempfile(tmpdir = tempfile())
-#' dir_name <- dirname(file_name)
-#' dir.create(dir_name)
-#' file.create(file_name)
+#' file <- tempfile()
+#' file.create(file)
 #'
-#' con <- file(file_name, "r+")
+#' con <- file(file, "r+")
 #' data <- c("01:00:56", "", "Test", "")
 #' writeLines(data, con = con)
 #' close(con)
 #'
 #' replace_in_file(
-#'   dir = dir_name,
+#'   file = file,
 #'   pattern = "([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]",
 #'   replacement = ""
-#'   )
+#' )
 #'
-#' con <- file(file_name, "r+")
+#' con <- file(file, "r+")
 #' readLines(con)
 #' close(con)
 replace_in_file <- function(
-    dir = clipr::read_clip(), #nolint
+    file, #nolint
     pattern,
     replacement
   ) {
-  checkmate::assert_string(dir)
-  checkmate::assert_directory_exists(dir)
+  checkmate::assert_string(file)
+  checkmate::assert_file_exists(file)
   checkmate::assert_string(pattern)
   checkmate::assert_string(replacement)
   prettycheck::assert_identical(pattern, replacement, type = "length")
 
-  files <- fs::dir_ls(dir, type = "file")
-
   cli::cli_progress_bar(
-    "Replacing patterns", total = length(files), clear = FALSE
+    "Replacing patterns",
+    total = length(file),
+    clear = FALSE
   )
 
-  for (i in files) {
-    con <- file(i, "r+")
-    data <- readLines(con, warn = FALSE)
+  for (i in file) {
+    data <- readr::read_lines(i)
 
     if (length(data) == 0) {
-      close(con)
       cli::cli_progress_update()
 
       next
     }
 
-    data <- stringr::str_replace_all(data, pattern, replacement)
-    write(data, file = con)
-    close(con)
+    for (j in seq_along(pattern)) {
+      data <- stringr::str_replace_all(
+        data,
+        pattern[j],
+        replacement[j]
+      )
+    }
+
+    readr::write_lines(data, i)
 
     cli::cli_progress_update()
   }
 
   cli::cli_progress_done()
 
-  invisible(NULL)
+  invisible()
 }
